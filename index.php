7e5e7d253cc13314a6554fb089df6c34852b8170
JensDeNijs <jensdenijs@hotmail.com>
2021-05-26 11:49:14 +0200

    Second Challenge

--- index.php
+++ index.php
@@ -12,8 +12,40 @@ class Beverage{
     public function getInfo(){
         return "This beverage is " . $this->temperature . " and " . $this->color;
     }
+
 }
+class Beer extends Beverage{
+    private string $name;
+    private float $alcoholPercentage;
+
+    public function __construct(string $color, float $price,string $name,float $alcoholPercentage, string $temperature = "cold"){
+        $this->name = $name;
+        $this->alcoholPercentage = $alcoholPercentage;
+        parent::__construct($color, $price, $temperature);
+
+    }
+
+    public function getAlcoholpercentage(){
+        return $this->alcoholPercentage . "% alcoholpercentage";
+    }
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getInfo(){
+        return parent::getInfo() . " and has a " . $this->alcoholPercentage . "% alcoholpercentage";
+    }
+}
+
 
 $cola = new Beverage("black", 2);
 
 echo $cola->getInfo();
+
+$duvel = new Beer("blond",3.5,"Duvel", 8.5);
+echo "<br>";
+echo $duvel->getInfo();
+echo "<br>";
+echo $duvel->getAlcoholpercentage();
+echo "<br>";
